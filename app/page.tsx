0ef714d521712e6fdff7dc7b32c22d68fc0073ae
Jenin82 <jenin8282@gmail.com>
2023-12-19 23:29:03 +0530

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <main
-            className="flex min-h-screen flex-col items-center justify-between p-24"
-        >
-            test
-			
+        <main className="hero min-h-screen">
+            <div className="hero-content flex-col md:flex-row-reverse">
+                <Image
+                    src="/hero.svg"
+					width={800}
+					height={800}
+                    className="rounded-lg"
+					alt="hero"
+                />
+                <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                        assumenda excepturi exercitationem quasi. In deleniti
+                        eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
         </main>
     );
 }
