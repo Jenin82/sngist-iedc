9edcd777fbf7e466c85720ba7444f848dd1f6c00
EDWIN BENNY <edwinang2002@gmail.com>
2023-12-22 13:54:29 +0530

    build fix

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,19 +1,21 @@
 import EventCard from "@/components/EventCard/EventCard";
 import React from "react";
-import data from "./data.json"
+import data from "./data.json";
 
 const page = () => {
   return (
     <div className="mt-20 px-20">
       <div className="flex flex-wrap gap-10 justify-center">
-      {data.events.map((event) => (
-                
-        <EventCard
-          name={event.name}
-          description={event.description}
-          image={event.image}
-          isOver={event.isOver}
-          isNew={event.isNew} link={event.link}        />
+        {data.events.map((event) => (
+          <EventCard
+            key={event.name}
+            name={event.name}
+            description={event.description}
+            image={event.image}
+            isOver={event.isOver}
+            isNew={event.isNew}
+            link={event.link}
+          />
         ))}
       </div>
     </div>
