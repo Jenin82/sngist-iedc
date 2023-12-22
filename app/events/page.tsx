806190204c98f6efb756bef57b38d20d23c110a2
EDWIN BENNY <edwinang2002@gmail.com>
2023-12-22 13:49:09 +0530

    events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import EventCard from "@/components/EventCard/EventCard";
+import React from "react";
+import data from "./data.json"
 
 const page = () => {
   return (
-	<div>page</div>
-  )
-}
+    <div className="mt-20 px-20">
+      <div className="flex flex-wrap gap-10 justify-center">
+      {data.events.map((event) => (
+                
+        <EventCard
+          name={event.name}
+          description={event.description}
+          image={event.image}
+          isOver={event.isOver}
+          isNew={event.isNew} link={event.link}        />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
