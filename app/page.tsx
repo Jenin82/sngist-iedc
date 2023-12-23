c18aae0d2dcd99c6c0ba5ed2ef12d3a3a74afe2c
Jenin82 <jenin8282@gmail.com>
2023-12-23 11:03:18 +0530

    hero text effect

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,26 @@ export default function Home() {
                         className="max-w-[300px] md:max-w-full pb-8"
                     />
                     <div className="text-8xl font-extrabold">
-                        <span className=" bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-violet-500">
-                            SNGIST IEDC
+                        <span className="inline-grid">
+                            <span
+                                className="pointer-events-none col-start-1 row-start-1 
+								bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] 
+								bg-clip-text blur-xl [transform:translate3d(0,0,0)] 
+								[-webkit-text-fill-color:transparent] 
+								before:content-[attr(data-text)] 
+								[@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+                                aria-hidden="true"
+                                data-text="SNGIST IEDC"
+                            ></span>
+                            <span
+                                className="[&amp;::selection]:text-base-content relative col-start-1 row-start-1 
+								bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] 
+								bg-clip-text [-webkit-text-fill-color:transparent] 
+								[&amp;::selection]:bg-blue-700/20 
+								[@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+                            >
+                                SNGIST IEDC
+                            </span>
                         </span>
                     </div>
                     <p className="py-10 md:py-6 text-justify">
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -12,9 +12,27 @@ const Navbar = () => {
                         href={"/"}
                     >
                         {/* <Image src="/logo.svg" width={150} height={50} alt="logo" /> */}
-                        <h3 className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-violet-500">
-                            SNGIST IEDC
-                        </h3>
+                        <span className="inline-grid">
+                            <span
+                                className="pointer-events-none col-start-1 row-start-1 
+								bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] 
+								bg-clip-text blur-xl [transform:translate3d(0,0,0)] 
+								[-webkit-text-fill-color:transparent] 
+								before:content-[attr(data-text)] 
+								[@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+                                aria-hidden="true"
+                                data-text="SNGIST IEDC"
+                            ></span>
+                            <span
+                                className="[&amp;::selection]:text-base-content relative col-start-1 row-start-1 
+								bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] 
+								bg-clip-text [-webkit-text-fill-color:transparent] 
+								[&amp;::selection]:bg-blue-700/20 
+								[@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
+                            >
+                                SNGIST IEDC
+                            </span>
+                        </span>
                     </Link>
                 </button>
             </div>
