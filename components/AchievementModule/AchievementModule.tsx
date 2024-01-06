2c8c7e861ca75c3c43bdbf6daf9e44834d1ff2cb
Alan0602 <alankanil1234@gmail.com>
2024-01-06 21:47:08 +0530

    refactor(AchievementModule):resize image

--- components/AchievementModule/AchievementModule.tsx
+++ components/AchievementModule/AchievementModule.tsx
@@ -10,18 +10,18 @@ type Props = {
 
 const AchievementModule = (props: Props) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure className="px-10 pt-10">
+    <div className="card  w-80 bg-base-100 shadow-xl">
+      <figure className="px-5 pt-5">
         <img
           src={props.image}
           alt={props.name}
           className="rounded-xl"
         />
       </figure>
-      <div className="card-body items-center text-center">
+      <div className="card-body items-center text-center pt-2">
         <h2 className="card-title">{props.name}</h2>
         <p>{props.description}</p>
-        <div className="card-actions border-t-2 w-full pt-5 justify-center flex items-center">
+        <div className="card-actions border-t-2 w-full pt-3  justify-center flex items-center">
         <MdDateRange />{props.date}
         </div>
       </div>
