a990bb6ecfce307fbbd64489cbcadb5d34145c44
Alan0602 <alankanil1234@gmail.com>
2024-01-13 20:01:17 +0530

    feat(Events):set api and date

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,19 +1,31 @@
 import EventCard from "@/components/EventCard/EventCard";
 import React from "react";
-import data from "./data.json";
+import { getData, iedcRoutes } from "@/services/Endpoints";
 
-const page = () => {
+const page = async () => {
+
+    const data:EventsData[] = await getData(iedcRoutes.events)
+    
+    function isDateInFuture(inputDate: string): boolean {
+        const currentDate = new Date();
+        const providedDate = new Date(inputDate);
+    
+        // Compare the provided date with the current date
+        return providedDate < currentDate;
+    }
+
     return (
         <div className="p-20 bg-base-200">
             <h1 className="text-4xl font-bold text-center py-6 mb-4">Events</h1>
             <div className="flex flex-wrap gap-10 justify-center">
-                {data.events.map((event) => (
+                {data.map((event) => (
                     <EventCard
                         key={event.name}
                         name={event.name}
                         description={event.description}
                         image={event.image}
-                        isOver={event.isOver}
+                        isOver={isDateInFuture(event.date)}
+                        date={event.date}
                         isNew={event.isNew}
                         link={event.link}
                     />
