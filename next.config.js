d977669e62066865ee37d37fd8a21a284ee32962
Jenin82 <jenin8282@gmail.com>
2024-03-13 20:43:35 +0530

    chore: made minor changes

--- next.config.js
+++ next.config.js
@@ -1,33 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com",
-                port: "",
-                pathname: "/u/**",
-            },
-            {
-                protocol: "https",
-                hostname: "i.imgur.com",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: "i.ibb.co",
-                port: "",
-                pathname: "/**/**",
-            },
-            {
-                protocol: "https",
-                hostname: "i.postimg.cc",
-                port: "",
-                pathname: "/**/**",
-            },
-        ],
-    },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				port: "",
+				pathname: "/u/**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "imgur.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.ibb.co",
+				port: "",
+				pathname: "/**/**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.postimg.cc",
+				port: "",
+				pathname: "/**/**",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig
